4fcbf6197ee0fbae764ad5739b3ebcdabeaef951
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-09 14:04:06 -0600

    Setup functions for Adding a Book to Library

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log(`Hello!`)
+//Array to track the books
+let myLibrary = [];
+
+//Object Contructor
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+//Prototype info function to display book information
+Book.prototype.info = function () {
+  return (
+    `${this.title} by ${this.author}, ${this.pages} pages, ` +
+    (this.isRead ? "already read" : "not read yet")
+  );
+};
+
+//Function to Add a book to the library array
+
+const addBookToLibrary = (book) => {
+  myLibrary.push(book);
+};
+
+//Testing the Function
+let theHobbit = new Book("The Hobbit", "J.R.R Tolken", 294, true);
+
+let theDrawingOfThree = new Book(
+  "The Drawing Of Three",
+  "Stephan King",
+  452,
+  false
+);
+
+addBookToLibrary(theHobbit);
+
+addBookToLibrary(theDrawingOfThree);
+
+console.log(myLibrary);
+
